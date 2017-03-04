e2631e22e691da4553a0ae09fdc3a3ab1fc3be46
Mateus R. Moreira <mateusrodriguesmoreira20@gmail.com>
2017-03-03 22:40:48 -0300

    json

---
+++ json/index.php
@@ -0,0 +1,10 @@
+
+<?php 
+require 'json.php';
+$requer = file_get_contents("json.php");
+
+echo $json;
+
+
+
+?>
---
+++ json/json.php
@@ -0,0 +1,12 @@
+<?php 
+	$json = array(
+			"cidade" => "Alexandria",
+			"Bairro" => "Periferia",
+			"cep" => 00
+		);
+
+	
+$json = json_encode($json);
+
+
+?>
